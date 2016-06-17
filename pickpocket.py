ef3225a5ad9702e6100f58169ddff681de820cee
Justin Li <justinnhli@gmail.com>
2016-06-16 18:06:19 -0700

    use file from argument in pickpocket

--- pickpocket.py
+++ pickpocket.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+import sys
+
 from bs4 import BeautifulSoup
 
-with open('/Users/justinnhli/Desktop/ril_export.html') as fd:
+with open(sys.argv[1]) as fd:
     soup = BeautifulSoup(fd.read(), 'html.parser')
 for tag in soup.find_all('ul')[0].find_all('a'):
     print(tag['href'])
